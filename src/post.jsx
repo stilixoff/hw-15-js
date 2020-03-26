59afdc7e4257bfdeb8ae1f84f75ab35ae0ea42b7
Oleh Starodubets <stilixoff@gmail.com>
2020-03-26 22:52:49 +0200

    done

--- src/post.jsx
+++ src/post.jsx
@@ -4,24 +4,31 @@ const Post = (props) => {
 const {author: {name, photo, nickname}, content, image, date} = props;
     return (   
         <div className='post'>
-            <div className='post_head'>
+            <div className='post_avatar' >
                 <div className='post_photo' >
                     <img className='photo' src= {photo} />
                 </div>
+            </div>
+            <div className='post_box'>
                 <div className='post_title' >
-                    <h2>{name}</h2>
-                    <h3>{nickname}</h3>
-                    <span>{date}</span>
-                    <p>{content}</p>
+                    <div className="post_name" >
+                        <h2>{name}</h2>
+                        <h3>{nickname}</h3>
+                        <h3>{date}</h3>
+                    </div>
+                    <div className="post_massage" >
+                        <p>{content}</p>
+                    </div>
+                </div>
+                <div className='post_content'>
+                    <img className="post_img" src={image} />
+                </div>
+                <div className="post_icon" >
+                    
                 </div>
             </div>
-            <div className='post_content'>
-                <img className="post_img" src={image} />
-            </div>
-
         </div>
-    
-)
+    )
 }
 
 export default Post;
