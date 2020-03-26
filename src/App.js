f667976037ba0369d92addebe87f91161e7e2996
Oleh Starodubets <stilixoff@gmail.com>
2020-03-26 21:51:17 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Post from './post';
+
+const ANAKIN_IMAGE = "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg";
+const RAY_IMAGE = "https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Post author={{
+      name: "Anakin Skywalker",
+      photo: ANAKIN_IMAGE,
+      nickname: "@dart_vader"
+    }}
+    content="WTF? Who is Ray? Why she is Skywalker? Luke...?"
+    image={RAY_IMAGE}
+    date={"26 февр."}
+  />
   );
 }
 
